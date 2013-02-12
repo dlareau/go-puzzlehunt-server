5a62b4b99721dbc22b3d5894daab10fa86684cc8
Alex Crichton <alex@alexcrichton.com>
2013-02-12 16:13:13 -0500

    Don't gzip websocket connections
    We'd need Hijack() to be implemented, and we'd have to continue to gzip after
    the Hijack which I don't want to look into...

--- utils/gzip.go
+++ utils/gzip.go
@@ -1,11 +1,9 @@
 package utils
 
 import "compress/gzip"
-import "net/http"
 import "io"
+import "net/http"
 import "strings"
-
-
 
 type gzipResponseWriter struct {
   sniffed bool
@@ -15,7 +13,9 @@ type gzipResponseWriter struct {
 
 func (w *gzipResponseWriter) Write(b []byte) (int, error) {
   if !w.sniffed {
-    w.ResponseWriter.Header().Set("Content-Type", http.DetectContentType(b))
+    if w.ResponseWriter.Header().Get("Content-Type") == "" {
+      w.ResponseWriter.Header().Set("Content-Type", http.DetectContentType(b))
+    }
     w.sniffed = true
   }
   return w.Writer.Write(b)
@@ -23,7 +23,8 @@ func (w *gzipResponseWriter) Write(b []byte) (int, error) {
 
 func GzipHandler(f http.Handler) http.Handler{
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+    if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") ||
+       strings.Contains(r.Header.Get("Connection"), "Upgrade") {
       f.ServeHTTP(w, r)
       return
     }
