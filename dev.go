89977c4fc13e413fd434cc16d0b0fc0d59c2b704
Alex Crichton <alex@alexcrichton.com>
2013-02-22 14:54:24 -0500

    Don't profile servers in production

--- dev.go
+++ dev.go
@@ -6,6 +6,7 @@ import "github.com/alexcrichton/go-paste"
 import _ "github.com/alexcrichton/go-paste/jsmin"
 import _ "github.com/alexcrichton/go-paste/sass"
 import _ "github.com/alexcrichton/go-paste/image"
+import _ "net/http/pprof"
 
 var PasteServer = paste.FileServer(paste.Config{
   Root: "./assets",
