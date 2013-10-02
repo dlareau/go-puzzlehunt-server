fa25f10075d0a8e86e07b5c243d26dd6243785e7
Will Crichton <willcrichton@cmu.edu>
2013-10-01 22:32:39 -0400

    Updated theme to hitchiker's

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 /* Authentication credentials for the admin/password reset pages */
 const AdminPassword = "shenanigans"
 const AdminRealm    = "Puzzlehunt Admin"
-const TeamRealm     = "Pirate Login"
+const TeamRealm     = "Frood Login"
 
 /* Database/server configuration */
 const MongoHost     = "mongodb://127.0.0.1:27017"
